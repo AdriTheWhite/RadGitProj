8f770326a97708be35699bd21901a21171c35cbb
arshad.shaikhp <arshad.shikhp@gmail.com>
2017-05-07 17:54:34 +0530

    First Time Commit

---
+++ src/org/ibm/idea/HelloWorldJava.java
@@ -0,0 +1,10 @@
+package org.ibm.idea;
+
+public class HelloWorldJava {
+	public static void main(String[] agrs) {
+		for(int i=1; i<=10; i++){
+			System.out.println("Hellow World : "+i);
+		}
+		
+	}
+}
